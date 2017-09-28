9b5a811ca18a465b480ce477d5e6dc6a6907db0c
ibrahim013 <walaibrahim13@gmail.com>
2017-09-28 16:19:54 +0100

    sortby amount and vendor feature added

--- imports/plugins/included/ui-search/client/templates/searchModal/searchFilter.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchFilter.js
@@ -4,22 +4,17 @@ import _ from "underscore";
 
 Template.searchFilter.helpers({
   getBrands(products) {
-    return _.uniq(_.pluck(products, "vendor"));
+    return _.uniq(_.pluck(products, vendor));
   }
 });
-
 Template.searchFilter.events({
-  "change #filter-by-price": function (event) {
-    Session.set("filterPrice", event.target.value);
+  "change #price-filter": function (event) {
+    Session.set("priceFilter", event.target.value);
   },
-  "change #filter-by-brand": function (event) {
-    Session.set("filterBrand", event.target.value);
-  },
-  "change #filter-by-latest": function (event) {
-    Session.set("filterLatest", event.target.value);
+  "change #brand-filter": function (event) {
+    Session.set("brandFilter", event.target.value);
   }
 });
-
 Template.sortByRange.events({
   "change #sort-value": function (event) {
     Session.set("sortValue", event.target.value);
