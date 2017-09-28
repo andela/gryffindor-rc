2a205a280a5d56a5b2a2330af9290ee10611299f
ibrahim013 <walaibrahim13@gmail.com>
2017-09-28 16:19:54 +0100

    sortby amount and vendor feature added

--- imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 import React from "react";
 import { DataType } from "react-taco-table";
 import { Template } from "meteor/templating";
+import { Session } from "meteor/session";
 import { i18next } from "/client/api";
 import { ProductSearch, Tags, OrderSearch, AccountSearch } from "/lib/collections";
 import { IconButton, SortableTable } from "/imports/plugins/core/ui/client/components";
@@ -43,24 +44,48 @@ Template.searchModal.onCreated(function () {
     }
   });
 
-
-  // Sort By Price Range
+  // Filter products by price
+  const priceFilter = (products, query) => {
+    return _.filter(products, (product) => {
+      if (product.price) {
+        const productMaxPrice = parseFloat(product.price.max);
+        const productMinPrice = parseFloat(product.price.min);
+        const queryMaxPrice = parseFloat(query[1]);
+        const queryMinPrice = parseFloat(query[0]);
+        if (productMinPrice >= queryMinPrice && productMaxPrice <= queryMaxPrice) {
+          return product;
+        }
+        return false;
+      }
+    });
+  };
+  // Sort products by price
   const sort = (products, type) => {
     return products.sort((a, b) => {
-      const productA = a.price === null ? -1 : a.price.min;
-      const productB = b.price === null ? -1 : a.price.min;
-      if (productA < productB) {
+      const A = a.price === null ? -1 : a.price.min;
+      const B = b.price === null ? -1 : b.price.min;
+      if (A < B) {
         return type === "DESC" ? 1 : -1;
-      } else if (productA > productB) {
+      } else if (A > B) {
         return type === "ASC" ? 1 : -1;
       }
       return 0;
     });
   };
 
+  // Filter products by brand
+  function brandFilter(products, query) {
+    return _.filter(products, (product) => {
+      return product.vendor === query;
+    });
+  }
+
   this.autorun(() => {
     const searchCollection = this.state.get("searchCollection") || "products";
     const searchQuery = this.state.get("searchQuery");
+    const priceQuery = Session.get("priceFilter");
+    const brandQuery = Session.get("brandFilter");
+    const sortQuery = Session.get("sortValue");
     const facets = this.state.get("facets") || [];
     const sub = this.subscribe("SearchResults", searchCollection, searchQuery, facets);
 
@@ -69,7 +94,19 @@ Template.searchModal.onCreated(function () {
        * Product Search
        */
       if (searchCollection === "products") {
-        const productResults = ProductSearch.find().fetch();
+        let productResults = ProductSearch.find().fetch();
+
+        if (!["null", "all"].includes(priceQuery) && priceQuery) {
+          const range = priceQuery.split("-");
+          productResults = priceFilter(productResults, range);
+        }
+        if (!["null", "all"].includes(brandQuery) && brandQuery) {
+          productResults = brandFilter(productResults, brandQuery);
+        }
+        if (sortQuery !== "null" && sortQuery) {
+          productResults = sort(productResults, sortQuery);
+        }
+
         const productResultsCount = productResults.length;
         this.state.set("productSearchResults", productResults);
         this.state.set("productSearchCount", productResultsCount);
@@ -161,6 +198,11 @@ Template.searchModal.helpers({
   },
   showSearchResults() {
     return false;
+  },
+  hasResults() {
+    const instance = Template.instance();
+    const sortResults = instance.state.get("productSearchResults").length;
+    return sortResults > 0;
   }
 });
 
@@ -198,6 +240,11 @@ Template.searchModal.events({
       Blaze.remove(view);
     });
   },
+  "click [data-event-action=filterClick]": function () {
+    // alert("conclude");
+    $("#searchFilter").toggleClass("hidden");
+    $("#toggleTags").toggleClass("hidden");
+  },
   "click [data-event-action=clearSearch]": function (event, templateInstance) {
     $("#search-input").val("");
     $("#search-input").focus();
@@ -216,7 +263,6 @@ Template.searchModal.events({
     templateInstance.state.set("searchCollection", searchCollection);
   }
 });
-
 
 /*
  * searchModal onDestroyed
