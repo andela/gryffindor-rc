a6b63d69db088fdd56a7a18fea981f2f20d196a7
ibrahim013 <walaibrahim13@gmail.com>
2017-09-28 16:19:54 +0100

    sortby amount and vendor feature added

--- imports/plugins/included/ui-search/client/templates/searchModal/searchFilter.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchFilter.js
@@ -1,6 +1,20 @@
 import { Session } from "meteor/session";
 import { Template } from "meteor/templating";
+import _ from "underscore";
 
+Template.searchFilter.helpers({
+  getBrands(products) {
+    return _.uniq(_.pluck(products, vendor));
+  }
+});
+Template.searchFilter.events({
+  "change #price-filter": function (event) {
+    Session.set("priceFilter", event.target.value);
+  },
+  "change #brand-filter": function (event) {
+    Session.set("brandFilter", event.target.value);
+  }
+});
 Template.sortByRange.events({
   "change #sort-value": function (event) {
     Session.set("sortValue", event.target.value);
