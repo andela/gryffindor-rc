f784669879d85429fbb2baa761f7c52e8ac6b801
Celoka <eloka.chima@gmail.com>
2017-10-03 13:52:59 +0100

    feature(fix bug): implement sort and filter search result
    - fix bug
    
    [Finishes #151297876]

--- imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
@@ -88,27 +88,6 @@ Template.searchModal.onCreated(function () {
     return products;
   };
 
-  // Sort By Price Range
-  const sort = (products, type) => {
-    return products.sort((a, b) => {
-      const A = a.price === null ? -1 : a.price.min;
-      const B = b.price === null ? -1 : b.price.min;
-      if (A < B) {
-        return type === "DESC" ? 1 : -1;
-      } else if (A > B) {
-        return type === "ASC" ? 1 : -1;
-      }
-      return 0;
-    });
-  };
-
-  // Filter products by brand
-  function brandFilter(products, query) {
-    return _.filter(products, (product) => {
-      return product.vendor === query;
-    });
-  }
-
   this.autorun(() => {
     const searchCollection = this.state.get("searchCollection") || "products";
     const searchQuery = this.state.get("searchQuery");
