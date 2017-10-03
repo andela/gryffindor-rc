3a8b5e925430d38eb0f6419ef7e6776d0aabe2b6
yamen <osaretinosayamen@outlook.com>
2017-10-03 08:31:27 +0100

    feature(content.js): add helper
    `` add helper function to loop over number of ratings ``
    
    Finishes  #151365761

--- imports/plugins/included/product-variant/client/templates/products/productGrid/content.js
+++ imports/plugins/included/product-variant/client/templates/products/productGrid/content.js
@@ -7,5 +7,12 @@ Template.gridContent.helpers({
     if (this.price && this.price.range) {
       return this.price.range;
     }
+  },
+  ratings: function (rating) {
+    const countArr = [];
+    for (let i = 0; i < rating; ++i) {
+      countArr.push({});
+    }
+    return countArr;
   }
 });
