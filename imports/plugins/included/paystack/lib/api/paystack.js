080ebebb942749e6ba57d679cb143ea933cb45df
yamen <osaretinosayamen@outlook.com>
2017-10-05 05:37:13 +0100

    feature(paystack.js): add paystack helper for verification
    Finishes #151641614

---
+++ imports/plugins/included/paystack/lib/api/paystack.js
@@ -0,0 +1,25 @@
+import request from "request";
+
+export const Paystack = {};
+
+const paystackHeaders = (secret) => {
+  return {
+    "Authorization": `Bearer ${secret}`,
+    "Content-Type": "application/json"
+  };
+};
+
+Paystack.verify = (reference, secret, callback) => {
+  const Id = reference;
+  const headers = paystackHeaders(secret);
+  const url = `https://api.paystack.co/transaction/verify/${Id}`;
+  request.get(url, { headers }, (err, response, body) => {
+    const result = JSON.parse(body);
+    if (result.status) {
+      callback(null, result);
+    } else {
+      callback(result, null);
+    }
+  });
+};
+
