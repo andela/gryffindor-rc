e0ef91e39c6cffcf76f5aaec59737d8f42c539b4
Nnamso <andeladeveloper@Nnamso.local>
2017-10-10 19:10:43 +0100

    resolve merge conflicts

--- imports/plugins/core/orders/client/templates/list/ordersList.js
+++ imports/plugins/core/orders/client/templates/list/ordersList.js
@@ -1,11 +1,7 @@
 import moment from "moment";
 import { Template } from "meteor/templating";
-<<<<<<< HEAD
 import { i18next } from "/client/api";
 import { Orders, Shops } from "/lib/collections";
-=======
-import { Orders, Shops} from "/lib/collections";
->>>>>>> feature/151297885/digital-products
 
 /**
  * dashboardOrdersList helpers
